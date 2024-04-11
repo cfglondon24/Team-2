918bc99998c2252e98f138a11bf5f62e2a79cba3
sophie939 <85836677+sophie939@users.noreply.github.com>
2024-04-11 15:27:47 +0100

    Add files via upload
    two arrays, one for right phrases to say to the user and one for wrong phrases

---
+++ feedback_chat_bot.py
@@ -0,0 +1,25 @@
+right_phrases = [
+    "Well done! You nailed it!",
+    "Great job! Keep up the good work!",
+    "Congratulations! You got it right!",
+    "Fantastic! You're on a roll!",
+    "Brilliant! You're a superstar!",
+    "Excellent! You're crushing it!",
+    "Superb! You're unstoppable!",
+    "Awesome! Keep up the amazing work!",
+    "Amazing! You're on the right track!",
+    "Way to go! You're acing it!"
+]
+
+wrong_phrases = [
+    "That's okay! Mistakes help us learn.",
+    "Don't worry! Keep trying your best!",
+    "No problem! Keep up the effort!",
+    "It's alright! You'll get it next time!",
+    "Keep going! You're doing great!",
+    "Keep trying! You're getting better!",
+    "Almost there! Keep pushing!",
+    "You're improving! Keep it up!",
+    "Don't give up! You're doing fine!",
+    "Keep your chin up! You're doing well!"
+]
